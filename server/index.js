685aa7d27e92cb17ccbf4cf9bd879d7c765822b1
‘Juliano <julianoperins@gmail.com>
2020-12-08 21:14:01 -0500

    first commit

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const FoodModel = require("./models/Food");
+
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://julianoperin:Hope2021@crud.utfeg.mongodb.net/food?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+  }
+);
+
+app.get("/", async (req, res) => {
+  const food = new FoodModel({ foodName: "Apple", daysSinceIAte: 3 });
+
+  try {
+    await food.save();
+    console.log("Inserted Data!");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.listen(3001, () => {
+  console.log("Server running on port 3001!");
+});
